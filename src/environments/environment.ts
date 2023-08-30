ac443742ba932f79a38b45d8a4aafb9b9274fe6f
TomkaYurii <tomka.yurii@gmail.com>
2023-08-30 14:32:42 +0300

    Angular 16 Firestore CRUD: add/get/update/delete documents with AngularFireStore

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDkPVeSRjPMwoCF4tT_PGQpCN2shrcu_3k",
+    authDomain: "angular16firebasecrud.firebaseapp.com",
+    projectId: "angular16firebasecrud",
+    storageBucket: "angular16firebasecrud.appspot.com",
+    messagingSenderId: "821363901553",
+    appId: "1:821363901553:web:2b0f2e367ad8556c7107d0",
+    measurementId: "G-PFVB8ZKC1M"
+  }
+};
